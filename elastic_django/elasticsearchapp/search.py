b4c3a1646918e720582ed9e7a088bc6455138aae
John Sangwon Suh <johnsuh94@gmail.com>
2017-11-04 22:48:24 +0900

    initial commit

---
+++ elastic_django/elasticsearchapp/search.py
@@ -0,0 +1,26 @@
+from elasticsearch_dsl import DocType, Text, Date
+from elasticsearch_dsl.connections import connections
+
+from . import models
+from elasticsearch import Elasticsearch
+from elasticsearch.helpers import bulk
+
+connections.create_connection(hosts=['localhost:9200'], timeout=20)
+
+
+class BlogPostIndex(DocType):
+    author = Text()
+    posted_date = Date()
+    title = Text()
+    text = Text()
+
+    class Meta:
+        index = 'blogpost-index'
+
+
+def bulk_indexing():
+    # initialize the model to be indexed
+    BlogPostIndex.init()
+    # pass Es to index the queryset from the model BlogPost as a generator
+    es = Elasticsearch()
+    bulk(client=es, actions=(blogpost.indexing() for blogpost in models.BlogPost.objects.all().iterator()))
